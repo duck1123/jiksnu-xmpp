97014f806389b715f139f91ae4a58f4e55691b44
Daniel E. Renfer <duck@kronkltd.net>
2014-05-10 09:42:55 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jiksnu-xmpp
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/jiksnu_xmpp/core_test.clj
@@ -0,0 +1,7 @@
+(ns jiksnu-xmpp.core-test
+  (:require [clojure.test :refer :all]
+            [jiksnu-xmpp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
